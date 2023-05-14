9d8c10594b7e33d9088eed88894aaeeb3fbaadc7
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-05-15 00:34:32 +0530

    sending friend requests done

---
+++ src/app/api/friends/add/route.js
@@ -0,0 +1,61 @@
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { redis } from "@/lib/redis";
+import { addFriendValidator } from "@/lib/validations/add-friend";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const { email: emailToAdd } = addFriendValidator.parse(body.email);
+
+    // console.log("emailtoadd", emailToAdd);
+    const idToAdd = await fetchRedis("get", `user:email:${emailToAdd}`);
+    // console.log("idtoadd", idToAdd);
+    if (!idToAdd) {
+      return new Response("This person does not exist", { status: 404 });
+    }
+
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    if (idToAdd === session.user.id) {
+      return new Response("You cannot add yourself as a friend", {
+        status: 400,
+      });
+    }
+
+    // if user already added
+    const isAlreadyAdded =
+      (await fetchRedis(
+        "sismember",
+        `user:${idToAdd}:incoming_friend_requests`,
+        session.user.id
+      )) === 1;
+    if (isAlreadyAdded) {
+      return new Response("You already sent request", { status: 400 });
+    }
+    const isAlreadyFriends =
+      (await fetchRedis(
+        "sismember",
+        `user:${session.user.id}:friends`,
+        idToAdd
+      )) === 1;
+    if (isAlreadyFriends) {
+      return new Response("You already added this person", { status: 400 });
+    }
+
+    // send friend request
+    redis.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
+    return new Response("OK");
+  } catch (err) {
+    console.error(err);
+    if (err instanceof z.ZodError) {
+      return new Response("invalid request payload", { status: 422 });
+    }
+    return new Response("Invalid Request", { status: 400 });
+  }
+}
