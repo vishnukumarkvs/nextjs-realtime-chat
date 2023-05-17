cb969d3e9b69b73d5e83f7419cb08de7f53f2d98
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-05-17 09:44:36 +0530

    fixed redis client bug

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
-  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,12 +1,6 @@
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
-import { redis } from "./redis";
 
 export function cn(...input) {
   return twMerge(clsx(...input));
-}
-
-export async function checkUserExists(email) {
-  const userString = await redis.get(`user:${email}`);
-  return userString !== null;
 }
