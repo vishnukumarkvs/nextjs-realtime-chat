27631d227fff619b081eeeb9ba3c897441b67e0d
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-05-28 10:09:58 +0530

    modified envs

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,14 +8,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCciS7lDrV5BDKfhk2MwZe6mJaAJU8yQuo",
-  authDomain: "nextjs-chat-272b3.firebaseapp.com",
-  databaseURL: "https://nextjs-chat-272b3-default-rtdb.firebaseio.com",
-  projectId: "nextjs-chat-272b3",
-  storageBucket: "nextjs-chat-272b3.appspot.com",
-  messagingSenderId: "624014183997",
-  appId: "1:624014183997:web:4732c7672047f633fae454",
-  measurementId: "G-F03K98V7VK",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
